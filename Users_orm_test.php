1b05cdf079479bdb0a37c2542dcf04cc2ed8b24b
mtyndall007 <mtyndall007@gmail.com>
2017-12-07 17:54:44 -0500

    Add files via upload

---
+++ Users_orm_test.php
@@ -0,0 +1,41 @@
+<?php
+require_once('Users_orm.php');
+
+function renderUsers($t) {
+  if (is_null($t)) {
+    print("No such user.");
+  } else {
+?>
+<ul>
+    <li>id = <?php print($t->getID())?></li>
+    <li>username = <?php print($t->getUsername())?></li>
+    <li>password = <?php print($t->getPassword())?></li>
+</ul>
+<?php
+      }
+}
+
+?>
+<h1>Transaction Test</h1>
+
+<h2>Retrieving by ID</h2>
+
+<?php
+
+$t = Users::findByID(1);
+?>
+<p>
+$t = Users::findByID(1);
+<blockquote>
+	<?php renderUsers($t); ?>
+</blockquote>
+
+<?php
+
+$t = Users::create(bob13, test);
+?>
+$t = Users::create(bob, test);
+
+<blockquote>
+	<?php renderUsers($t); ?>
+</blockquote>

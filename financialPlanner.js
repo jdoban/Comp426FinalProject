fcaf48c8cdc8671880aa021d86500403b2a12509
jdoban <33875637+jdoban@users.noreply.github.com>
2017-12-10 21:40:21 -0500

    Add files via upload

--- financialPlanner.js
+++ financialPlanner.js
@@ -1,10 +1,17 @@
 $(document).ready(function (){
 
+var k=0;
+var l=0;
+
 $("#addAnotherLoan").on('click', function(e){
 addAnotherLoan();
 });
-var k=0;
-var l=0;
+
+$("#addAnotherPayment").on('click', function(e){
+addAnotherPayment();
+});
+
+
 var addAnotherLoan =function(){
 	var original=document.getElementById('firstLoan' + k);
 	var clone=original.cloneNode(true);
@@ -16,30 +23,34 @@ var addAnotherLoan =function(){
 	original.parentNode.appendChild(loans);
 	original.parentNode.appendChild(clone);
 	var addButton = document.getElementById('addAnotherLoan');
+	var submitButton=document.getElementById('submitDebts');
 	clone.parentNode.appendChild(addButton);
+	clone.parentNode.appendChild(submitButton);
+
 	children= clone.children;
 	children[0].reset();
 }
 
 
-$("#addAnotherPayment").on('click', function(e){
-addAnotherPayment();
-});
 var addAnotherPayment =function(){
-	var original=document.getElementById('firstPayment' + k);
-	var clone=original.cloneNode(true);
-	clone.id= "firstPayment" + ++l; ++j;
-	var loan= document.getElementById('first-debt');
-	var loans=loan.cloneNode(true);
-	var j =l+1;
-	loans.innerHTML="Payment "+ j+".";
-	original.parentNode.appendChild(loans);
-	original.parentNode.appendChild(clone);
-	var addButton = document.getElementById('addAnotherPayment');
-	clone.parentNode.appendChild(addButton);
-	children= clone.children;
-	children[0].reset();
+	var original1=document.getElementById('firstPayment' + l);
+	var clone1=original1.cloneNode(true);
+	clone1.id= "firstPayment" + ++l; ++m;
+	var loan1= document.getElementById('firstRecurring');
+	var loans1=loan1.cloneNode(true);
+	var m =l+1;
+	loans1.innerHTML="Payment "+ m+".";
+	original1.parentNode.appendChild(loans1);
+	original1.parentNode.appendChild(clone1);
+	var addButton1 = document.getElementById('addAnotherPayment');
+	var submitButton1=document.getElementById('submitPayments');
+	clone1.parentNode.appendChild(addButton1);
+	clone1.parentNode.appendChild(submitButton1);
+
+	children1= clone1.children;
+	children1[0].reset();
 }
+
 });
 
 

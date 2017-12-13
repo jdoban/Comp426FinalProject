b9af2635a17eec8a9fa88aa23b9757ec58b4ec33
jdoban <33875637+jdoban@users.noreply.github.com>
2017-12-13 04:56:10 -0500

    Add files via upload

--- RecommendedBudget.js
+++ RecommendedBudget.js
@@ -1,115 +1,142 @@
-$(document).ready(function ()
-{
+$(document).ready(function (){
 
-	var base_url = "https://wwwp.cs.unc.edu/Courses/comp426-f17/users/mtyndall/finalproject";
+var base_url = "https://wwwp.cs.unc.edu/Courses/comp426-f17/users/mtyndall/finalproject";
 
-	function PlannedFunction() 
-	{
-		var x = document.getElementById("PlannedEvent");
-		var y = document.getElementById("SpentEvent");
-		var z = document.getElementById("RemainingEvent");
-		if (x.style.display === "none") 
-		{
-			x.style.display = "block";
-			y.style.display = "none";
-			z.style.display = "none";
-		} 
-	}
-
-	function SpentFunction() 
-	{
-		var x = document.getElementById("PlannedEvent");
-		var y = document.getElementById("SpentEvent");
-		var z = document.getElementById("RemainingEvent");
-		if (y.style.display === "none") 
-		{
-			x.style.display = "none";
-			y.style.display = "block";
-			z.style.display = "none";
-		} 
-	}
-
-	function RemainingFunction() 
-	{
-		var x = document.getElementById("PlannedEvent");
-		var y = document.getElementById("SpentEvent");
-		var z = document.getElementById("RemainingEvent");
-		if (z.style.display === "none") 
-		{
-			x.style.display = "none";
-			y.style.display = "none";
-			z.style.display = "block";
-		} 
-	}
-
-	var getUser=function(user)
-	{
-		var name= user+"=";
-		var decodedCookie=decodeURIComponent(document.cookie);
-		var ca = decodedCookie.split(';');
-		for(var i=0;i<ca.length;i++)
-		{
-			var c=ca[i];
-			while(c.charAt(0)==' ')
-			{
-				c=c.substring(1);
-			}
-			if(c.indexOf(name)==0)
-			{
-				return c.substring(name.length, c.length);
-			}
-		} 
-		return "";
-	}
-	
-	var user_id=getUser("user");
-
-	var all_recurring_payments;
-
-	$.ajax(base_url + "/FindRPByID.php/" + user_id,
-	{
-		type: "GET",
-		dataType: "json",
-
-		success: function(response, status, jqXHR) 
-		{
-			console.log(JSON.stringify(response));
-			all_recurring_payments=response;
+var getUser=function(user){
+	var name= user+"=";
+	var decodedCookie=decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for(var i=0;i<ca.length;i++){
+		var c=ca[i];
+		while(c.charAt(0)==' '){
+			c=c.substring(1);
 		}
-	}
-	)
+		if(c.indexOf(name)==0){
+			return c.substring(name.length, c.length);
+		}
+	} 
+	return "";
+}
+var user_id=getUser("user");
+var allDebt;
+var income=0;
+var allSavings=0;
+var transportation;
+var food;
+var utilities;
+var housing;
+var recreation;
+var personal;
+var insurance;
+var health;
+var other;
+var username;
+var totalDebt=0;
+
+$.ajax(base_url + "/FindRPByID.php/" + user_id,
+	       {type: "GET",
+		       dataType: "json",
+		       async: false,
+		       success: function(response, status, jqXHR) {
+            transportation=response.Transportation;
+			food=response.Food;
+			utilities=response.Utilities;
+			housing=response.Housing;
+			recreation=response.Recreation;
+			personal=response.Personal;
+			insurance=response.Insurance;
+			health=response.Health;
+			other=response.Other;
+		       },
+		       error: function(jqXHR){
+		       	console.log(jqXHR);
+		       }
+           }
+       )
+
+$.ajax(base_url + "/FindDebtByID.php/" + user_id,
+	       {type: "GET",
+		       dataType: "json",
+		       async: false,
+		       success: function(response, status, jqXHR) {
+		       	console.log(response);
+             allDebt=response;
+		       },
+		       error: function(jqXHR){
+		       	console.log(jqXHR);
+		       }
+           }
+       )
+$.ajax(base_url + "/Users.php/" + user_id,
+	       {type: "GET",
+		       dataType: "json",
+		       success: function(response, status, jqXHR) {
+		       	username=response.username;
+		       	document.getElementById('welcome').innerHTML="Welcome, "+ username;
+
+			       },
+		       error: function(jqXHR){
+		       	console.log(jqXHR);
+		       }
+           }
+       )
+$.ajax(base_url + "/Users.php/" + user_id,
+	       {type: "GET",
+		       dataType: "json",
+		       success: function(response, status, jqXHR) {
+		       	income=response.income;
+			       },
+		       error: function(jqXHR){
+		       	console.log(jqXHR);
+		       }
+           }
+       )
+$.ajax(base_url + "/FindSavingsByID.php/" + user_id,
+	       {type: "GET",
+		       dataType: "json",
+		       success: function(response, status, jqXHR) {
+             allSavings=response;
+		       },
+		       error: function(jqXHR){
+		       	console.log(jqXHR);
+		       }
+           }
+       )
+	for (var key in allDebt){
+		totalDebt+=allDebt[key];
+}
+
+    	
+    document.getElementById("PHoN").innerHTML = housing;
 
 	var total_expenses = (housing+insurance+transportation+food+utilities+recreation+personal+health+totalDebt+other);
-	var total_expenses2 = (housing+insurance+transportation+food+utilities+recreation+personal+health+totalDebt+other);
 
-    document.getElementById("PHoN").innerHTML = "$" + housing;
-    document.getElementById("PIN").innerHTML = "$" + insurance;
+    document.getElementById("PHoN").innerHTML = "$" + housing.toFixed(2);
+    document.getElementById("PIN").innerHTML = "$" + insurance.toFixed(2);
 
-    document.getElementById("PSN").innerHTML = "$" + (income-total_expenses)/2;
-
-    document.getElementById("PTN").innerHTML = "$" + transportation;
-    document.getElementById("PFN").innerHTML = "$" + food;
-    document.getElementById("PUN").innerHTML = "$" + utilities;
-    document.getElementById("PRN").innerHTML = "$" + recreation;
-    document.getElementById("PPN").innerHTML = "$" + personal;
-    document.getElementById("PHeN").innerHTML = "$" + health;
-    document.getElementById("PDN").innerHTML = "$" + totalDebt;
-    document.getElementById("PlannedSum").innerHTML = "$" + total_expenses;
+    document.getElementById("PTN").innerHTML = "$" + (total_expenses*2*((1-(housing/total_expenses)-(insurance/total_expenses))/9)).toFixed(2);
+    document.getElementById("PFN").innerHTML = "$" + (total_expenses*2*((1-(housing/total_expenses)-(insurance/total_expenses))/9)).toFixed(2);
+    document.getElementById("PUN").innerHTML = "$" + (total_expenses*1*((1-(housing/total_expenses)-(insurance/total_expenses))/9)).toFixed(2);
+    document.getElementById("PRN").innerHTML = "$" + (total_expenses*1*((1-(housing/total_expenses)-(insurance/total_expenses))/9)).toFixed(2);
+    document.getElementById("PPN").innerHTML = "$" + (total_expenses*2*((1-(housing/total_expenses)-(insurance/total_expenses))/9)).toFixed(2);
+    document.getElementById("PHeN").innerHTML = "$" + (total_expenses*1*((1-(housing/total_expenses)-(insurance/total_expenses))/9)).toFixed(2);
+    document.getElementById("PDN").innerHTML = "$" + totalDebt.toFixed(2);
+    document.getElementById("PlannedSum").innerHTML = "$" + total_expenses.toFixed(2);
 
     
 
-    document.getElementById("PHoP").innerHTML = "(" + 100*(housing/total_expenses) + "%)";
-    document.getElementById("PIP").innerHTML = "(" + 100*(insurance/total_expenses) + "%)";
+    document.getElementById("PHoP").innerHTML = "(" + (100*(housing/total_expenses)).toFixed(2) + "%)";
+    document.getElementById("PIP").innerHTML = "(" + (100*(insurance/total_expenses)).toFixed(2) + "%)";
 
-    document.getElementById("PSP").innerHTML = "(" + ((income-total_expenses)/2)/(((income-total_expenses)/2)+total_expenses) "%)";
+    document.getElementById("PTP").innerHTML = "(" + (200*((1-(housing/total_expenses)-(insurance/total_expenses))/9)).toFixed(2) + "%)";
+    document.getElementById("PFP").innerHTML = "(" + (200*((1-(housing/total_expenses)-(insurance/total_expenses))/9)).toFixed(2) + "%)";
+    document.getElementById("PUP").innerHTML = "(" + (100*((1-(housing/total_expenses)-(insurance/total_expenses))/9)).toFixed(2) + "%)";
+    document.getElementById("PRP").innerHTML = "(" + (100*((1-(housing/total_expenses)-(insurance/total_expenses))/9)).toFixed(2) + "%)";
+    document.getElementById("PPP").innerHTML = "(" + (200*((1-(housing/total_expenses)-(insurance/total_expenses))/9)).toFixed(2) + "%)";
+    document.getElementById("PHeP").innerHTML = "(" + (100*((1-(housing/total_expenses)-(insurance/total_expenses))/9)).toFixed(2) + "%)";
+    document.getElementById("PDP").innerHTML = "(" + (100*(totalDebt/total_expenses)).toFixed(2) + "%)";
 
-    document.getElementById("PTP").innerHTML = "(" + 200*((1-(housing/total_expenses)-(insurance/total_expenses))/9) + "%)";
-    document.getElementById("PFP").innerHTML = "(" + 200*((1-(housing/total_expenses)-(insurance/total_expenses))/9) + "%)";
-    document.getElementById("PUP").innerHTML = "(" + 100*((1-(housing/total_expenses)-(insurance/total_expenses))/9) + "%)";
-    document.getElementById("PRP").innerHTML = "(" + 100*((1-(housing/total_expenses)-(insurance/total_expenses))/9) + "%)";
-    document.getElementById("PPP").innerHTML = "(" + 200*((1-(housing/total_expenses)-(insurance/total_expenses))/9) + "%)";
-    document.getElementById("PHeP").innerHTML = "(" + 100*((1-(housing/total_expenses)-(insurance/total_expenses))/9) + "%)";
-    document.getElementById("PDP").innerHTML = "(" + 100*(totalDebt/total_expenses) + "%)";
+    document.getElementById("note").innerHTML = "Your current total savings is $" + allSavings.toFixed(2) + ". It is recommended that you set aside 15% of your monthly income";
 
 
 
@@ -125,15 +152,14 @@ google.charts.load('current', {'packages':['corechart']});
         data.addColumn('string', 'Attribute');
         data.addColumn('number', 'Percentage');
         data.addRows([
-        	['Savings', ((income-total_expenses)/2)],
-        	['Transportation', transportation],
-        	['Food', food],
-        	['Utilities', utilities],
+        	['Transportation', total_expenses*2*((1-(housing/total_expenses)-(insurance/total_expenses))/9)],
+        	['Food', total_expenses*2*((1-(housing/total_expenses)-(insurance/total_expenses))/9)],
+        	['Utilities', total_expenses*1*((1-(housing/total_expenses)-(insurance/total_expenses))/9)],
         	['Housing', housing],
-        	['Recreation', recreation],
-        	['Personal', personal],
+        	['Recreation', total_expenses*1*((1-(housing/total_expenses)-(insurance/total_expenses))/9)],
+        	['Personal', total_expenses*2*((1-(housing/total_expenses)-(insurance/total_expenses))/9)],
         	['Insurance', insurance],
-        	['Health', health],
+        	['Health', total_expenses*1*((1-(housing/total_expenses)-(insurance/total_expenses))/9)],
         	['Debt', totalDebt]
         	]);
         // Set chart options
@@ -144,8 +170,4 @@ google.charts.load('current', {'packages':['corechart']});
         chart.draw(data, options);
     }
 
-	
-
-
-}
-);
+});
